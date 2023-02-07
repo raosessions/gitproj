459557c0d7b6dc93d2a3fe71f52fc5689921b4c8
saitej1124 <saitej1124@gmail.com>
2023-02-07 17:25:08 -0600

    control statement

---
+++ Rec.java
@@ -0,0 +1,15 @@
+public class Rec {
+    public void ifElse(){
+
+        int x=5;
+        if (x>10)
+        {
+            System.out.println("print x  is smaller ");
+        }
+        else {
+            System.out.println("print x  is bigger ");
+        }
+
+    }
+    
+}
