a8100328bafdffe349a85c4b04bf8d30b13f7fdc
Lucky19802 <naveensadineni23@gmail.com>
2023-02-07 22:20:20 -0600

    reverse function added

--- Naveen/string_project.java
+++ Naveen/string_project.java
@@ -1,17 +1,24 @@
 public class String_Project{
     public static void main(String[] args){
         String s = "This is a java class";
-        System.out.println(s.length());
-        System.out.println(s.toUpperCase());
+        System.out.println("The string length is :"+s.length());
+        System.out.println("String in upper case was :"+s.toUpperCase());
+        String rev_string = "";
+        int n = s.length();
+        for(int i=0; i<s.length(); i++){
+            char lastchar = s.charAt(n-1);
+            rev_string += lastchar;
+            n--;
+        }
+        System.out.println("Reverse string :"+rev_string);
         String[] total_words = s.split(" ");
-        System.out.println(total_words.length);
-        int count=0;
+        System.out.println("Total words in a string was :"+total_words.length);
         for(int i=0; i<total_words.length;i++){
-            System.out.println(total_words[i].length());
+            System.out.println("\'"+total_words[i]+"\' length was :"+total_words[i].length());
         }
         int Start_index = s.indexOf("java");
-        System.out.println(Start_index);
+        System.out.println("Index of word 'java' in a string was :"+Start_index);
         String result = s.replace("java", "SQL");
-        System.out.println(result);
+        System.out.println("After replacing the string was :"+result);
     }  
 }
